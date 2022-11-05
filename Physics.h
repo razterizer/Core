8035ad1725447e2fc98d6be756640dcfa65fdd9e
Rasmus <scratty@gmail.com>
2022-11-05 13:48:28 +0100

    Physics.h * Adding new namespace physics. * Adding new namespace UnitConversion under physics with existing functions moved into it.

--- Physics.h
+++ Physics.h
@@ -1,11 +1,20 @@
 #pragma once
 
-float deg_celcius_to_kelvin(float degC)
+namespace physics
 {
-  return degC + 273.15f;
-}
+  namespace UnitConversion
+  {
+    float deg_celcius_to_kelvin(float degC)
+    {
+      return degC + 273.15f;
+    }
+    
+    float kelvin_to_deg_celcius(float K)
+    {
+      return K - 273.15f;
+    }
+  }
 
-float kelvin_to_deg_celcius(float K)
-{
-  return K - 273.15f;
+  
+
 }

93624e9ad09a45597526a7ddb05732f9aa1d0324
Rasmus <scratty@gmail.com>
2022-11-14 18:52:06 +0100

    Added LeftInteger mode to str::adjust_str().

--- StringHelper.h
+++ StringHelper.h
@@ -11,23 +11,26 @@ namespace str
     return ret;
   }
   
-  enum class Adjustment { Left, Center, Right };
-  std::string adjust_str(const std::string& str, Adjustment adj, int width, char empty_char = ' ')
+  enum class Adjustment { Left, Center, Right, LeftInteger };
+  std::string adjust_str(const std::string& str, Adjustment adj, int width, int start_idx = 0, char empty_char = ' ')
   {
     if (str.size() > width)
       return str.substr(0, width);
+    
+    auto offset_str = rep_char(empty_char, start_idx);
   
     int remaining = width - static_cast<int>(str.size());
     switch (adj)
     {
-      case Adjustment::Left: return str + rep_char(empty_char, remaining);
+      case Adjustment::Left: return offset_str + str + rep_char(empty_char, remaining);
       case Adjustment::Center:
       {
+        std::string result;
         if (width % 2 == 0)
         {
           int remain_half = remaining / 2;
           auto spaces = rep_char(empty_char, remain_half);
-          return spaces + str + spaces;
+          result = offset_str + spaces + str + spaces;
         }
         else
         {
@@ -36,10 +39,37 @@ namespace str
           int remain_right = (remaining + 1) / 2;
           auto spaces_left = rep_char(empty_char, remain_left);
           auto spaces_right = rep_char(empty_char, remain_right);
-          return spaces_left + str + spaces_right;
+          result = offset_str + spaces_left + str + spaces_right;
         }
+        result = result.substr(0, width);
+        return result;
       }
-      case Adjustment::Right: return rep_char(empty_char, remaining) + str;
+      case Adjustment::Right:
+      {
+        auto result = offset_str + rep_char(empty_char, remaining) + str;
+        result = result.substr(0, width);
+        return result;
+      }
+      case Adjustment::LeftInteger:
+      {
+        int number_idx = static_cast<int>(str.size()) - 1;
+        for (int i = 0; i <= 9; ++i)
+        {
+          auto needle = std::to_string(i);
+          auto idx = static_cast<int>(str.find(needle));
+          math::minimize<int>(number_idx, idx, 0, str.size());
+        }
+        //  0               s          w
+        // "bla:            1234       "
+        auto result = rep_char(empty_char, width);
+        for (size_t i = 0; i < std::min(start_idx, number_idx); ++i)
+          result[i] = str[i];
+        for (size_t i = 0; i < str.length(); ++i)
+          if (start_idx + i < width && number_idx + i < str.length())
+            result[start_idx + i] = str[number_idx + i];
+        result = result.substr(0, width);
+        return result;
+      }
     }
   }
 }

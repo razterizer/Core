946c5721a74a8aab7fb43727586cd9bfdf90f4c6
Rasmus <scratty@gmail.com>
2022-11-17 09:21:12 +0100

    MarkovChain: * Now the markov chain is a map of vectors instead of a map of maps. * Adding a function add_transitions() that takes a vector of items. * Adding a function import_transitions() that given a file name imports the items from a text file. * Adding a function print() that prints the states and transitions. * Sorting the transitions by weight which I think could give a better random selection behaviour. * Adding min/max num items arguments to generate which give a much better control of the output. * Fixing bug in generate() where the for loop just continued after the next state item had been found and set which gave incorrect transitional behaviour. * Subtracting the random value from each weight to give more correct random selection behaviour.

--- MarkovChain.h
+++ MarkovChain.h
@@ -7,7 +7,10 @@
 
 #pragma once
 #include <map>
-#include <optional>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iostream>
 #include "Rand.h"
 
 namespace markov_chain
@@ -17,7 +20,7 @@ namespace markov_chain
   class MarkovChain
   {
   public:
-    using MarkovChainTransitionTable = std::map<T, std::map<T, float>>;
+    using MarkovChainTransitionTable = std::map<T, std::vector<std::pair<T, float>>>;
     MarkovChainTransitionTable mctt;
     T empty_item;
     
@@ -26,11 +29,65 @@ namespace markov_chain
     void add_transition(T from, T to)
     {
       auto& targets = mctt[from];
-      auto it = targets.find(to);
+      auto it = std::find_if(targets.begin(), targets.end(), [&to](const auto& ipair) { return ipair.first == to; });
       if (it != targets.end())
         it->second++;
       else
-        targets[to]++;
+        targets.emplace_back(to, 1);
+    }
+    
+    void add_transitions(const std::vector<T>& items)
+    {
+      auto N = static_cast<int>(items.size());
+      if (N >= 2)
+      {
+        for (int i = 0; i < N - 1; ++i)
+        {
+          T from = items[i];
+          T to = items[i+1];
+          add_transition(from, to);
+        }
+      }
+      if (N > 0)
+        add_transition(items[N-1], empty_item);
+    }
+    
+    int import_transitions(const std::string& filename)
+    {
+      std::ifstream input(filename);
+      if (input.fail())
+      {
+        std::cerr << "Error opening file \"" + filename + "\"!" << std::endl;
+        return EXIT_FAILURE;
+      }
+      std::stringstream ss;
+      ss << input.rdbuf();
+      input.close();
+      
+      std::string line;
+      while (std::getline(ss, line))
+      {
+        // Break line into words.
+        std::istringstream iss(line);
+        std::vector<std::string> word_vec;
+        std::string word;
+        while (iss >> word)
+          word_vec.emplace_back(word);
+        
+        add_transitions(word_vec);
+      }
+      
+      return EXIT_SUCCESS;
+    }
+    
+    void print() const
+    {
+      for (const auto& state : mctt)
+      {
+        std::cout << " [" << state.first << "] :" << std::endl;
+        for (const auto& trg : state.second)
+          std::cout << "   => " << trg.first << " : " << trg.second << std::endl;
+      }
     }
   
     void normalize_transition_weights()
@@ -45,44 +102,58 @@ namespace markov_chain
           for (auto& trg : targets.second)
             trg.second /= tot;
         }
+        std::sort(targets.second.begin(), targets.second.end(),
+                  [](const auto& A, const auto& B) { return A.second < B.second; });
       }
     }
     
-    T generate()
+    T generate(int min_num_items = -1, int max_num_items = -1) const
     {
       auto N = mctt.size();
       if (N == 0)
         return empty_item;
-      auto rnd_idx = rnd::randomize_int(0, static_cast<int>(N) - 1);
-      int ctr = 0;
-      const T* start_item_ptr = nullptr;
-      for (auto& item : mctt)
-      {
-        if (rnd_idx == ctr++)
-        {
-          start_item_ptr = &item.first;
-          break;
-        }
-      }
-      if (start_item_ptr == nullptr)
-        return empty_item;
       
-      T item = *start_item_ptr;
-      
-      T ret = item;
-      while (item != "")
+      size_t num_items = 1;
+      T ret = empty_item;
+      do
       {
-        auto t = rnd::calc_t_rnd();
-        const auto& entry = mctt[item];
-        for (const auto& trg : entry)
+        num_items = 1;
+        auto rnd_idx = rnd::randomize_int(0, static_cast<int>(N) - 1);
+        int ctr = 0;
+        const T* start_item_ptr = nullptr;
+        for (auto& item : mctt)
         {
-          if (t < trg.second)
+          if (rnd_idx == ctr++)
           {
-            item = trg.first;
-            ret += item;
+            start_item_ptr = &item.first;
+            break;
           }
         }
-      }
+        if (start_item_ptr == nullptr)
+          return empty_item;
+        
+        T item = *start_item_ptr;
+        
+        ret = item;
+        while (item != "")
+        {
+          auto t = rnd::calc_t_rnd();
+          const auto& entry = mctt.at(item);
+          for (const auto& trg : entry)
+          {
+            if (t < trg.second)
+            {
+              item = trg.first;
+              ret += item;
+              if (item != "")
+                num_items++;
+              break;
+            }
+            t -= trg.second;
+          }
+        }
+      } while ((min_num_items != -1 && num_items < min_num_items) || (max_num_items != -1 && num_items > max_num_items));
+
       
       return ret;
     }

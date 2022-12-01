553123e86a0e4f66f7586e2b8901d32ef6298355
Rasmus <scratty@gmail.com>
2022-12-01 11:30:04 +0100

    MachineLearning_tests.h: * Removing unecessary namespace specifier ml::ann::. * Fixing compilation issue if attempting to build the previous commit, the reason being missing full rename of print_result() into print_output() in all places.

--- Tests/MachineLearning_tests.h
+++ Tests/MachineLearning_tests.h
@@ -16,7 +16,7 @@ namespace ml
   
     void unit_tests()
     {
-      ml::ann::Neuron n({ 1.1, 1, 0.7, 1, 1 }, 0.5, ml::ann::PhiType::GELU);
+      Neuron n({ 1.1, 1, 0.7, 1, 1 }, 0.5, PhiType::GELU);
       n.set_inputs({ -0.61, 0.75, 2.6, -1.5, -1 });
       std::cout << '\n';
       n.update_forward();
@@ -30,32 +30,32 @@ namespace ml
       
       std::cout << "== Neural Layer ==\n";
       
-      ml::ann::NeuralLayer nl { { { 1.1, 1, 0.7, 1, 1 }, { 0.3, 1.2, 3.2, 0.75, 0.1 } }, { 0.5, -0.21 }, ml::ann::PhiType::GELU };
+      NeuralLayer nl { { { 1.1, 1, 0.7, 1, 1 }, { 0.3, 1.2, 3.2, 0.75, 0.1 } }, { 0.5, -0.21 }, PhiType::GELU };
       nl.set_inputs({ -0.61, 0.75, 2.6, -1.5, -1 });
       for (int i = 0; i < 20; ++i)
         nl.train({ 1, 0 });
       nl.print();
       
-      ml::ann::NeuralNetwork nn
+      NeuralNetwork nn
       {
         {
           // Ni*No = 5x4
-          ml::ann::generate_weights(ml::ann::GenWeightsType::Rndn_m1_p1, 5, 4),
+          generate_weights(GenWeightsType::Rndn_m1_p1, 5, 4),
           // Ni*No = 4x2
-          ml::ann::generate_weights(ml::ann::GenWeightsType::Rndn_m1_p1, 4, 2),
+          generate_weights(GenWeightsType::Rndn_m1_p1, 4, 2),
         },
         { // No = 4, 2
-          generate_biases(ml::ann::GenWeightsType::Zero, 4),
-          generate_biases(ml::ann::GenWeightsType::Zero, 2)
+          generate_biases(GenWeightsType::Zero, 4),
+          generate_biases(GenWeightsType::Zero, 2)
         },
-        { ml::ann::PhiType::Leaky_ReLU, ml::ann::PhiType::Tanh }
+        { PhiType::Leaky_ReLU, PhiType::Tanh }
       };
       
       nn.set_inputs({ -0.61, 0.75, 2.6, -1.5, -1 });
       for (int i = 0; i < 1'000; ++i)
         nn.train({ 1, 0 }, 0.02, 0.5);
       nn.print();
-      nn.print_output();
+      nn.print_result();
 
     }
   

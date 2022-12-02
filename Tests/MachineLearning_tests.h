b545b2ab5c74532a88caf1b00fbe228b3dddacba
Rasmus <scratty@gmail.com>
2022-12-02 19:37:54 +0100

    * Adding unit test for activation_1d(). * Adding unit tests for conv_1d().

--- Tests/MachineLearning_tests.h
+++ Tests/MachineLearning_tests.h
@@ -73,23 +73,95 @@ namespace ml
       
       // CNN
       
-      auto p0_1d = pooling_1d({ 1, 0, -2, 7, 3, 9, 1 }, PoolingType::Max, 2, 2, false);
+      auto x = std::vector<float> { 1, 0, -2, 7, 3, 9, 1 };
+      auto p0_1d = pooling_1d(x, PoolingType::Max, 2, 2, false);
       assert((p0_1d == std::vector<float> { 1, 7, 9 }));
       
-      auto p1_1d = pooling_1d({ 1, 0, -2, 7, 3, 9, 1 }, PoolingType::Max, 2, 2, true);
+      auto p1_1d = pooling_1d(x, PoolingType::Max, 2, 2, true);
       assert((p1_1d == std::vector<float> { 1, 7, 9, 1 }));
       
-      auto p2_1d = pooling_1d({ 1, 0, -2, 7, 3, 9, 1 }, PoolingType::Min, 3, 1, false);
+      auto p2_1d = pooling_1d(x, PoolingType::Min, 3, 1, false);
       assert((p2_1d == std::vector<float> { -2, -2, -2, 3, 1 }));
       
-      auto p3_1d = pooling_1d({ 1, 0, -2, 7, 3, 9, 1 }, PoolingType::Min, 3, 1, true);
+      auto p3_1d = pooling_1d(x, PoolingType::Min, 3, 1, true);
       assert((p3_1d == std::vector<float> { -2, -2, -2, 3, 1 }));
       
-      auto p4_1d = pooling_1d({ 1, 0, -2, 7, 3, 9, 1 }, PoolingType::Mean, 2, 1, false);
+      auto p4_1d = pooling_1d(x, PoolingType::Mean, 2, 1, false);
       assert((p4_1d == std::vector { 0.5f, -1.f, 2.5f, 5.f, 6.f, 5.f }));
       
       auto af_p4_1d = activation_1d(p4_1d, PhiType::ReLU);
       assert((af_p4_1d == std::vector { 0.5f, 0.f, 2.5f, 5.f, 6.f, 5.f }));
+      
+      auto kernel = std::vector<float> { 1, 2, 3 };
+      auto c0_1d = conv_1d(x, kernel, 1.f, ConvType::Full);
+      assert((c0_1d == std::vector<float> { 2, 3, 2, 4, 12, 37, 29, 30, 4 }));
+      
+      auto f = std::vector<float> { 0, 0, -1, 2, 1, 0, 1, 2, 3, 0, 0 };
+      auto g = std::vector<float> { 0, 1, -1, 1, 0 };
+      auto c1_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c1_1d == std::vector<float> { 0, 0, 0, -1, 3, -2, 1, 2, 1, 2, -1, 3, 0, 0, 0 }));
+      auto c2_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c2_1d == std::vector<float> { 0, -1, 3, -2, 1, 2, 1, 2, -1, 3, 0 }));
+      auto c3_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c3_1d == std::vector<float> { 3, -2, 1, 2, 1, 2, -1 }));
+      g = std::vector<float> { 0, 1, 2, 3 };
+      auto c4_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c4_1d == std::vector<float> { 0, 0, 0, -1, 0, 2, 8, 4, 4, 10, 12, 9, 0, 0 }));
+      auto c5_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c5_1d == std::vector<float> { 0, -1, 0, 2, 8, 4, 4, 10, 12, 9, 0 }));
+      auto c6_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c6_1d == std::vector<float> { -1, 0, 2, 8, 4, 4, 10, 12 }));
+      g = std::vector<float> { 1, -1, -2 };
+      auto c7_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c7_1d == std::vector<float> { 0, 0, -1, 3, 1, -5, -1, 1, -1, -7, -6, 0, 0 }));
+      auto c8_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c8_1d == std::vector<float> { 0, -1, 3, 1, -5, -1, 1, -1, -7, -6, 0 }));
+      auto c9_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c9_1d == std::vector<float> { -1, 3, 1, -5, -1, 1, -1, -7, -6 }));
+      g = std::vector<float> { -1, 5 };
+      auto c10_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c10_1d == std::vector<float> { 0, 0, 1, -7, 9, 5, -1, 3, 7, 15, 0, 0 }));
+      auto c11_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c11_1d == std::vector<float> { 0, 1, -7, 9, 5, -1, 3, 7, 15, 0, 0 }));
+      auto c12_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c12_1d == std::vector<float> { 0, 1, -7, 9, 5, -1, 3, 7, 15, 0 }));
+      g = std::vector<float> { 4 };
+      auto c13_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c13_1d == std::vector<float> { 0, 0, -4, 8, 4, 0, 4, 8, 12, 0, 0 }));
+      auto c14_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c14_1d == std::vector<float> { 0, 0, -4, 8, 4, 0, 4, 8, 12, 0, 0 }));
+      auto c15_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c15_1d == std::vector<float> { 0, 0, -4, 8, 4, 0, 4, 8, 12, 0, 0 }));
+      f = std::vector<float> { 0, 0, -1, 2, 1, 0, 1, 2, 3, 0, 0, 1 };
+      g = std::vector<float> { 0, 1, -1, 1, 0 };
+      auto c16_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c16_1d == std::vector<float> { 0, 0, 0, -1, 3, -2, 1, 2, 1, 2, -1, 3, 1, -1, 1, 0 }));
+      auto c17_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c17_1d == std::vector<float> { 0, -1, 3, -2, 1, 2, 1, 2, -1, 3, 1, -1 }));
+      auto c18_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c18_1d == std::vector<float> { 3, -2, 1, 2, 1, 2, -1, 3 }));
+      g = std::vector<float> { 1, -1 };
+      auto c19_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c19_1d == std::vector<float> { 0, 0, -1, 3, -1, -1, 1, 1, 1, -3, 0, 1, -1 }));
+      auto c20_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c20_1d == std::vector<float> { 0, -1, 3, -1, -1, 1, 1, 1, -3, 0, 1, -1 }));
+      auto c21_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c21_1d == std::vector<float> { 0, -1, 3, -1, -1, 1, 1, 1, -3, 0, 1 }));
+      f = std::vector<float> { -7, 0, -1, 2 };
+      g = std::vector<float> { 0, 1, -1, 1, 0 };
+      auto c22_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c22_1d == std::vector<float> { 0, -7, 7, -8, 3, -3, 2, 0 }));
+      auto c23_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c23_1d == std::vector<float> { 7, -8, 3, -3 }));
+      auto c24_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c24_1d == std::vector<float> {}));
+      f = std::vector<float> { -7, 0, -1 };
+      auto c25_1d = conv_1d(f, g, 0, ConvType::Full);
+      assert((c25_1d == std::vector<float> { 0, -7, 7, -8, 1, -1, 0 }));
+      auto c26_1d = conv_1d(f, g, 0, ConvType::Same);
+      assert((c26_1d == std::vector<float> { 7, -8, 1 }));
+      auto c27_1d = conv_1d(f, g, 0, ConvType::Valid);
+      assert((c27_1d == std::vector<float> {}));
     }
   
   }

2b2d3e52abe4a70b00e0df804675f359fddeb77a
Rasmus <scratty@gmail.com>
2022-12-07 09:37:08 +0100

    WIP implementation of RBFLayer.

--- MachineLearning/ann_dense.h
+++ MachineLearning/ann_dense.h
@@ -299,6 +299,119 @@ namespace ml
           n->print();
       }
     };
+  
+    //
+  
+    class RBFLayer
+    {
+      const size_t Ni = 0;
+      const size_t No = 0;
+      std::vector<std::vector<float>> weights;
+      std::vector<float> biases;
+      std::vector<Input> inputs;
+      std::vector<float> outputs;
+      
+      std::vector<std::vector<float>> w_diff_prev;
+      
+    public:
+      RBFLayer(const std::vector<std::vector<float>>& w,
+               const std::vector<float>& b)
+        : No(w.size()), Ni(w[0].size())
+        , weights(w), biases(b)
+        , outputs(No)
+      {}
+      
+      void set_inputs(const std::vector<Input>& x)
+      {
+        inputs = x;
+      }
+      
+      void update_forward()
+      {
+        const auto nan = std::numeric_limits<float>::quiet_NaN();
+        for (size_t o_idx = 0; o_idx < No; ++o_idx)
+        {
+          auto& o = outputs[o_idx];
+          o = 0.f;
+          const auto& w_o = weights[o_idx];
+          for (size_t i_idx = 0; i_idx < Ni; ++i_idx)
+            o += math::sq(inputs[i_idx].get().value_or(nan) - w_o[i_idx]);
+        }
+      }
+      
+      // Back-prop
+      // y_trg : target output.
+      // eta : learning rate (0.1).
+      // mu : momentum term (0.05).
+      // r : random term for simulated annealing-ish behaviour (0).
+      // diff = -eta * grad + mu * diff_prev + r
+      // Returns gradient.
+      std::vector<std::vector<float>> update_backward(const std::vector<float>& y_trg,
+                                                      float eta = 0.1f, float mu = 0.05f, float r = 0.f)
+      {
+        const auto nan = std::numeric_limits<float>::quiet_NaN();
+        assert(y_trg.size() == No);
+        std::vector<std::vector<float>> grad(No);
+        // dC/dwi = dC/df * df/dwi
+        for (size_t o_idx = 0; o_idx < No; ++o_idx)
+        {
+          auto err_diff = -(y_trg[o_idx] - outputs[o_idx]);
+          auto dC_df = err_diff;
+          auto& w_o = weights[o_idx];
+          std::vector<float> df_dw(Ni);
+          for (size_t i_idx = 0; i_idx < Ni; ++i_idx)
+            df_dw[i_idx] = 2*(inputs[i_idx].get().value_or(nan) - w_o[i_idx]);
+          auto dC_dw = stlutils::mult_scalar(df_dw, dC_df);
+          
+          auto w_diff = stlutils::mult_scalar(dC_dw, -eta);
+          w_diff = stlutils::add(w_diff, stlutils::mult_scalar(w_diff_prev[o_idx], mu));
+          w_diff = stlutils::add_scalar(w_diff, r);
+          w_o = stlutils::add(w_o, w_diff);
+          w_diff_prev[o_idx] = w_diff;
+          grad[o_idx] = dC_dw;
+        }
+        return grad;
+      }
+      
+      // Forward-prop followed by a back-prop.
+      // y_trg : target output.
+      // eta : learning rate (0.1).
+      // mu : momentum term (0.05).
+      // r : random term for simulated annealing-ish behaviour (0).
+      // diff = -eta * grad + mu * diff_prev + r
+      // Returns gradient.
+      std::vector<std::vector<float>> train(const std::vector<float>& y_trg,
+                                            float eta = 0.1f, float mu = 0.05f, float r = 0.f)
+      {
+        update_forward();
+        return update_backward(y_trg, eta, mu, r);
+      }
+      
+      const std::vector<Input> output() const
+      {
+        std::vector<Input> ret(No);
+        for (size_t o_idx = 0; o_idx < No; ++o_idx)
+          ret[o_idx] = outputs[o_idx];
+        return ret;
+      }
+      
+      const std::vector<float> result() const
+      {
+        return outputs;
+      }
+    
+      size_t num_inputs() const { return Ni; }
+      size_t num_outputs() const { return No; }
+    
+      void print_result() const
+      {
+        std::cout << "[ ";
+        std::cout << outputs.front();
+        for (size_t o_idx = 1; o_idx < No; ++o_idx)
+          std::cout << ", " << outputs[o_idx];
+        std::cout << " ]\n";
+      }
+    };
 
     //
 

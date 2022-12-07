e238786e8c4281f9b2923585abc70d327cbccc31
Rasmus <scratty@gmail.com>
2022-12-07 17:14:02 +0100

    * Turning RBFLayer into a network class now called RBFNetwork since it contains an input layer, a hidden layer and an output layer (so basically two layers, since the input layer is usually not counted in). * The RBFNetwork::update_forward() has now been updated to include the hidden gaussian connections. * Added some ascii-art in an attempt att explaining the layout of the RBFNetwork class and use it to understand how to design the forward/backard propagation routines and overall API.

--- MachineLearning/ann_dense.h
+++ MachineLearning/ann_dense.h
@@ -316,119 +316,6 @@ namespace ml
     };
   
     //
-  
-    class RBFLayer
-    {
-      const size_t Ni = 0;
-      const size_t No = 0;
-      std::vector<std::vector<float>> weights;
-      std::vector<float> biases;
-      std::vector<Input> inputs;
-      std::vector<float> outputs;
-      
-      std::vector<std::vector<float>> w_diff_prev;
-      
-    public:
-      RBFLayer(const std::vector<std::vector<float>>& w,
-               const std::vector<float>& b)
-        : No(w.size()), Ni(w[0].size())
-        , weights(w), biases(b)
-        , outputs(No)
-      {}
-      
-      void set_inputs(const std::vector<Input>& x)
-      {
-        inputs = x;
-      }
-      
-      void update_forward()
-      {
-        const auto nan = std::numeric_limits<float>::quiet_NaN();
-        for (size_t o_idx = 0; o_idx < No; ++o_idx)
-        {
-          auto& o = outputs[o_idx];
-          o = 0.f;
-          const auto& w_o = weights[o_idx];
-          for (size_t i_idx = 0; i_idx < Ni; ++i_idx)
-            o += math::sq(inputs[i_idx].get().value_or(nan) - w_o[i_idx]);
-        }
-      }
-      
-      // Back-prop
-      // y_trg : target output.
-      // eta : learning rate (0.1).
-      // mu : momentum term (0.05).
-      // r : random term for simulated annealing-ish behaviour (0).
-      // diff = -eta * grad + mu * diff_prev + r
-      // Returns gradient.
-      std::vector<std::vector<float>> update_backward(const std::vector<float>& y_trg,
-                                                      float eta = 0.1f, float mu = 0.05f, float r = 0.f)
-      {
-        const auto nan = std::numeric_limits<float>::quiet_NaN();
-        assert(y_trg.size() == No);
-        std::vector<std::vector<float>> grad(No);
-        // dC/dwi = dC/df * df/dwi
-        for (size_t o_idx = 0; o_idx < No; ++o_idx)
-        {
-          auto err_diff = -(y_trg[o_idx] - outputs[o_idx]);
-          auto dC_df = err_diff;
-          auto& w_o = weights[o_idx];
-          std::vector<float> df_dw(Ni);
-          for (size_t i_idx = 0; i_idx < Ni; ++i_idx)
-            df_dw[i_idx] = 2*(inputs[i_idx].get().value_or(nan) - w_o[i_idx]);
-          auto dC_dw = stlutils::mult_scalar(df_dw, dC_df);
-          
-          auto w_diff = stlutils::mult_scalar(dC_dw, -eta);
-          w_diff = stlutils::add(w_diff, stlutils::mult_scalar(w_diff_prev[o_idx], mu));
-          w_diff = stlutils::add_scalar(w_diff, r);
-          w_o = stlutils::add(w_o, w_diff);
-          w_diff_prev[o_idx] = w_diff;
-          grad[o_idx] = dC_dw;
-        }
-        return grad;
-      }
-      
-      // Forward-prop followed by a back-prop.
-      // y_trg : target output.
-      // eta : learning rate (0.1).
-      // mu : momentum term (0.05).
-      // r : random term for simulated annealing-ish behaviour (0).
-      // diff = -eta * grad + mu * diff_prev + r
-      // Returns gradient.
-      std::vector<std::vector<float>> train(const std::vector<float>& y_trg,
-                                            float eta = 0.1f, float mu = 0.05f, float r = 0.f)
-      {
-        update_forward();
-        return update_backward(y_trg, eta, mu, r);
-      }
-      
-      const std::vector<Input> output() const
-      {
-        std::vector<Input> ret(No);
-        for (size_t o_idx = 0; o_idx < No; ++o_idx)
-          ret[o_idx] = outputs[o_idx];
-        return ret;
-      }
-      
-      const std::vector<float> result() const
-      {
-        return outputs;
-      }
-    
-      size_t num_inputs() const { return Ni; }
-      size_t num_outputs() const { return No; }
-    
-      void print_result() const
-      {
-        std::cout << "[ ";
-        std::cout << outputs.front();
-        for (size_t o_idx = 1; o_idx < No; ++o_idx)
-          std::cout << ", " << outputs[o_idx];
-        std::cout << " ]\n";
-      }
-    };
-
-    //
 
     class NeuralNetwork
     {
@@ -549,6 +436,154 @@ namespace ml
         }
       }
     };
+  
+    //
+    
+    //                                                                                    w00
+    // x0         +-> |[X] - [C0]| --> phi(|[X]-[C0]|) = exp(-|[X]-[C0]|^2/(2*sigma_0^2)) ----->y_0
+    //           /                                                                       w01 // w02
+    // x1 = [X] +---> |[X] - [C1]| --> phi(|[X]-[C1]|) = exp(-|[X]-[C1]|^2/(2*sigma_1^2)) -+ /
+    //           \                                                                          /
+    // x2         +-> |[X] - [C2]| --> phi(|[X]-[C2]|) = exp(-|[X]-[C2]|^2/(2*sigma_2^2)) -+  -> y_1
+    //
+    // x3
+    //
+    // [Ck] = [ck0, ck1, ck2, ck3]
+    //
+    // y_0 = sum(w_0j * phi_j(), j=0..2);
+    // y_1 = sum(w_1j * phi_j(), j=0..2);
+    //
+    // Nk = Ninputs = 4. Here Ni.
+    // Nj = Nhidden = 3. Here Nh.
+    // Ni = Noutput = 2. Here No.
+    class RBFNetwork
+    {
+      const size_t Ni = 0;
+      const size_t Nh = 0;
+      const size_t No = 0;
+      std::vector<Input> inputs; // Ni
+      std::vector<std::vector<float>> centroids; // Nh x Ni
+      std::vector<float> distances; // Nh
+      std::vector<float> sigma;  // Nh
+      std::vector<float> phi; // Nh
+      std::vector<std::vector<float>> weights; // No x Nh
+      std::vector<float> outputs; // No
+    
+      std::vector<std::vector<float>> w_diff_prev;
+    
+    public:
+      RBFNetwork(const std::vector<std::vector<float>>& c,
+               const std::vector<float>& s,
+               const std::vector<std::vector<float>>& w)
+        : No(w.size()), Nh(s.size()), Ni(c[0].size())
+        , centroids(c), distances(Nh), sigma(s), phi(Nh), weights(w)
+        , outputs(No)
+      {}
+    
+      void set_inputs(const std::vector<Input>& x)
+      {
+        assert(Ni == x.size());
+        inputs = x;
+      }
+    
+      void update_forward()
+      {
+        const auto nan = std::numeric_limits<float>::quiet_NaN();
+        for (size_t h_idx = 0; h_idx < Nh; ++h_idx)
+        {
+          const auto& c_h = centroids[h_idx];
+          auto& dist_h = distances[h_idx];
+          dist_h = 0;
+          for (size_t i_idx = 0; i_idx < Ni; ++i_idx)
+            dist_h += math::sq(inputs[i_idx].get().value_or(nan) - c_h[i_idx]);
+          dist_h = std::sqrt(dist_h); // Optional?
+        }
+        for (size_t h_idx = 0; h_idx < Nh; ++h_idx)
+          phi[h_idx] = std::exp(-0.5f*math::sq(distances[h_idx]/sigma[h_idx]));
+        for (size_t o_idx = 0; o_idx < No; ++o_idx)
+        {
+          auto o = outputs[o_idx];
+          o = 0;
+          const auto& w_o = weights[o_idx];
+          for (size_t h_idx = 0; h_idx < Nh; ++h_idx)
+            o += phi[h_idx]*w_o[h_idx];
+        }
+      }
+    
+      // Back-prop
+      // y_trg : target output.
+      // eta : learning rate (0.1).
+      // mu : momentum term (0.05).
+      // r : random term for simulated annealing-ish behaviour (0).
+      // diff = -eta * grad + mu * diff_prev + r
+      // Returns gradient.
+      std::vector<std::vector<float>> update_backward(const std::vector<float>& y_trg,
+                                                      float eta = 0.1f, float mu = 0.05f, float r = 0.f)
+      {
+        const auto nan = std::numeric_limits<float>::quiet_NaN();
+        assert(y_trg.size() == No);
+        std::vector<std::vector<float>> grad(No);
+        // dC/dwi = dC/dy * dy/dwi
+        for (size_t o_idx = 0; o_idx < No; ++o_idx)
+        {
+          auto err_diff = -(y_trg[o_idx] - outputs[o_idx]);
+          auto dC_dy = err_diff;
+          auto& w_o = weights[o_idx];
+          std::vector<float> dy_dw(Ni);
+          for (size_t i_idx = 0; i_idx < Ni; ++i_idx)
+            dy_dw[i_idx] = 2*(inputs[i_idx].get().value_or(nan) - w_o[i_idx]);
+          auto dC_dw = stlutils::mult_scalar(dy_dw, dC_dy);
+    
+          auto w_diff = stlutils::mult_scalar(dC_dw, -eta);
+          w_diff = stlutils::add(w_diff, stlutils::mult_scalar(w_diff_prev[o_idx], mu));
+          w_diff = stlutils::add_scalar(w_diff, r);
+          w_o = stlutils::add(w_o, w_diff);
+          w_diff_prev[o_idx] = w_diff;
+          grad[o_idx] = dC_dw;
+        }
+        return grad;
+      }
+    
+      // Forward-prop followed by a back-prop.
+      // y_trg : target output.
+      // eta : learning rate (0.1).
+      // mu : momentum term (0.05).
+      // r : random term for simulated annealing-ish behaviour (0).
+      // diff = -eta * grad + mu * diff_prev + r
+      // Returns gradient.
+      std::vector<std::vector<float>> train(const std::vector<float>& y_trg,
+                                            float eta = 0.1f, float mu = 0.05f, float r = 0.f)
+      {
+        update_forward();
+        return update_backward(y_trg, eta, mu, r);
+      }
+    
+      const std::vector<Input> output() const
+      {
+        std::vector<Input> ret(No);
+        for (size_t o_idx = 0; o_idx < No; ++o_idx)
+          ret[o_idx] = outputs[o_idx];
+        return ret;
+      }
+    
+      const std::vector<float> result() const
+      {
+        return outputs;
+      }
+    
+      size_t num_inputs() const { return Ni; }
+      size_t num_outputs() const { return No; }
+    
+      void print_result() const
+      {
+        std::cout << "[ ";
+        std::cout << outputs.front();
+        for (size_t o_idx = 1; o_idx < No; ++o_idx)
+          std::cout << ", " << outputs[o_idx];
+        std::cout << " ]\n";
+      }
+    };
+
   
   }
 

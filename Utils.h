08f83b6f543e7b7b01acbfd37b70b7923c9fc7cd
Rasmus Anthin <scratty@gmail.com>
2024-05-20 10:02:01 +0200

    Adding new header file Utils.h that include wrappers for sscanf() that should work on both windows and linmac (hopefully).

---
+++ Utils.h
@@ -0,0 +1,32 @@
+//
+//  Utils.h
+//  Core Lib
+//
+//  Created by Rasmus Anthin on 2024-05-20.
+//
+
+
+#pragma once
+#include <cstdarg>  // For va_list, va_start, va_end
+#include <cstdio>   // For sscanf and sscanf_s
+
+namespace utils
+{
+
+  int sscanf(const char* str, const char* format, ...)
+  {
+    va_list args;
+    va_start(args, format);
+    int result;
+
+#ifdef _WIN32
+    result = ::vsscanf_s(str, format, args);
+#else
+    result = ::vsscanf(str, format, args);
+#endif
+
+    va_end(args);
+    return result;
+  }
+
+}

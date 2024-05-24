5db43b64f7e5432045a1b2a378e040e4d32f6e23
Rasmus Anthin <scratty@gmail.com>
2024-05-24 20:56:16 +0200

    build.sh: * Removing the option to build for windows on linmac as it is very problematic to find dlls for different 3rdparty libs for the build. * Simplifying the build script so that you don't need to supply the option "l" or "linmac" for building on linmac. * Since it now only supports building for linmac target, it now puts the executable in a new folder called "bin". So you can just go ahead and remove the folders "bin_linux" and "bin_windows" since they are no longer in use.

--- build.sh
+++ build.sh
@@ -31,57 +31,22 @@ EOF
 function build_linux_macos()
 {
   echo "Building for Linux / MacOS target..."
-  mkdir -p bin_linux
+  mkdir -p bin
   
-  build_cmd="g++ $1.cpp -o ./bin_linux/$1 -std=c++2a -O2 $(pkg_config_flags) $2"
+  build_cmd="g++ $1.cpp -o ./bin/$1 -std=c++2a -O2 $(pkg_config_flags) $2"
   
   # Generate compile database before building, so that clangd can also highlight build errors.
-  generate_clangd_compile_commands "$build_cmd" bin_linux "$1.cpp"
+  generate_clangd_compile_commands "$build_cmd" bin "$1.cpp"
 
   echo $build_cmd
   $build_cmd
 
-  echo "Done."
-}
-
-function build_windows()
-{
-  echo "Building for Windows target via mingw..."
-  mkdir -p bin_windows
-  build_cmd="x86_64-w64-mingw32-g++ -Wl,--stack,10000000 $1.cpp -o ./bin_windows/$1.exe -std=c++2a -O3 $2"
-  echo $build_cmd
-  $build_cmd
-  echo "Done."
-
-  echo "Copying Windows/mingw related dlls..."
-  # Paths to the found DLLs
-  #DLL_PATH_1="/System/Volumes/Data/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-x86_64/x86_64-w64-mingw32/lib/libstdc++-6.dll"
-  #DLL_PATH_2="/System/Volumes/Data/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-x86_64/x86_64-w64-mingw32/lib/libgcc_s_seh-1.dll"
-  #/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-i686/i686-w64-mingw32/bin/libwinpthread-1.dll
-  #/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-x86_64/x86_64-w64-mingw32/bin/libwinpthread-1.dll
-  DLL_PATH_1="/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-x86_64/x86_64-w64-mingw32/lib/libstdc++-6.dll"
-  DLL_PATH_2="/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-x86_64/x86_64-w64-mingw32/lib/libgcc_s_seh-1.dll"
-  DLL_PATH_3="/opt/homebrew/Cellar/mingw-w64/11.0.1/toolchain-x86_64/x86_64-w64-mingw32/bin/libwinpthread-1.dll"
-
-  if [ -f "$DLL_PATH_1" ] && [ -f "$DLL_PATH_2" ] && [ -f "$DLL_PATH_3" ]; then
-    cp "$DLL_PATH_1" ./bin_windows
-    cp "$DLL_PATH_2" ./bin_windows
-    cp "$DLL_PATH_3" ./bin_windows
-  else
-    echo "DLLs not found. Please provide correct paths."
-  fi
   echo "Done."
 }
 
 function show_help() {
     echo "Usage: ./<build_script> [output_name] [option]"
     echo "Options:"
-    echo "  all          : Builds for Linux/MacOS and Windows"
-    echo "  a            : Same as 'all'"
-    echo "  linmac       : Builds for Linux/MacOS only"
-    echo "  l            : Same as 'linmac'"
-    echo "  windows      : Builds for Windows only"
-    echo "  w            : Same as 'windows'"
     echo "  help         : Shows help explaining command line arguments"
     echo "  h            : Same as 'help'"
 }
@@ -92,24 +57,15 @@ if [ "$#" -lt 1 ]; then
     exit 1
 fi
 
-option=${2:-help}
+option=${2:-}
 optional_flags=${3:-}
 
 case "$option" in
-    'all' | 'a')
-        build_linux_macos "$1" "$optional_flags"
-        build_windows "$1" "$optional_flags"
-        ;;
-    'linmac' | 'l')
-        build_linux_macos "$1" "$optional_flags"
-        ;;
-    'windows' | 'w')
-        build_windows "$1" "$optional_flags"
-        ;;
     'help' | 'h')
         show_help
         ;;
     *)
-        echo "Invalid option. Use './<build_script> help' to see available options."
+        #echo "Invalid option. Use './<build_script> help' to see available options."
+        build_linux_macos "$1" "$optional_flags"
         ;;
 esac

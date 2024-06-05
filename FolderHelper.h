a9456848b13dda4438a7bbd068862550861f8e9b
Rasmus Anthin <scratty@gmail.com>
2024-06-05 21:31:17 +0200

    FolderHelper.h:  * Adding a bunch of useful functions:    - get_pwd() shorthand for std::filesystem::current_path().    - get_path_separator() gets the file path separator for the system where the code is run under.    - split_path().    - join_path().    - format_path() formats the path to use path separators of the current system (Win or LinMac).    - split_file_path().    - join_file_path().    - split_filename_ext().    - join_filename_ext().

--- FolderHelper.h
+++ FolderHelper.h
@@ -6,9 +6,11 @@
 //
 
 #pragma once
+#include "StringHelper.h"
 #ifdef _WIN32
 #include <windows.h>
 #endif
+#include <filesystem>
 
 
 namespace folder
@@ -39,5 +41,68 @@ namespace folder
     return path;
   }
 #endif
+
+  std::string get_pwd()
+  {
+    return std::filesystem::current_path();
+  }
+  
+  char get_path_separator()
+  {
+#ifdef _WIN32
+    return '\\';
+#else
+    return '/';
+#endif
+  }
+  
+  std::vector<std::string> split_path(const std::string& path)
+  {
+    return str::tokenize(path, { '/', '\\' });
+  }
+  
+  std::string join_path(const std::vector<std::string>& path_parts)
+  {
+    size_t num_parts = path_parts.size();
+    if (num_parts == 0)
+      return "";
+    std::string ret = path_parts[0];
+    for (size_t i = 1; i < num_parts; ++i)
+      ret += get_path_separator() + path_parts[i];
+    return ret;
+  }
+  
+  std::string format_path(const std::string& path)
+  {
+    return join_path(split_path(path));
+  }
+  
+  std::pair<std::string, std::string> split_file_path(const std::string& file_path)
+  {
+    auto idx = file_path.find_last_of("/\\");
+    if (idx != std::string::npos)
+      return { file_path.substr(0, idx), file_path.substr(idx + 1) };
+    // Here we assume that we passed a filename without path as argument.
+    return { "", file_path };
+  }
+  
+  std::string join_file_path(const std::pair<std::string, std::string>& path_and_filename)
+  {
+    return path_and_filename.first + get_path_separator() + path_and_filename.second;
+  }
+  
+  std::pair<std::string, std::string> split_filename_ext(const std::string& filename)
+  {
+    auto idx = filename.find_last_of(".");
+    if (idx != std::string::npos)
+      return { filename.substr(0, idx), filename.substr(idx + 1) }; // filename & ext.
+    // Here we assume that we passed a filename (path) without extension as argument.
+    return { filename, "" };
+  }
+  
+  std::string join_filename_ext(const std::pair<std::string, std::string>& basefilename_and_ext)
+  {
+    return basefilename_and_ext.first + '.' + basefilename_and_ext.second;
+  }
 
 }

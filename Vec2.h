b969a2d1ecd9d7f3f3c7b73a9405d7dc10da5d33
Rasmus Anthin <scratty@gmail.com>
2024-10-30 16:01:42 +0100

    Vec2.h: * Moving Termin8or version of Vec2 to here with some modifications (removing dependency to RC.h).

--- Vec2.h
+++ Vec2.h
@@ -1,12 +1,78 @@
 //
 //  Vec2.h
-//  Core
+//  Termin8or
 //
-//  Created by Rasmus Anthin on 2024-10-30.
+//  Created by Rasmus Anthin on 2024-10-25.
 //
 
-#ifndef Vec2_h
-#define Vec2_h
+#pragma once
 
 
-#endif /* Vec2_h */
+struct Vec2
+{
+  float r = 0.f;
+  float c = 0.f;
+  
+  Vec2() = default;
+  
+  Vec2(float rr, float cc)
+    : r(rr)
+    , c(cc)
+  {}
+  
+  Vec2(const Vec2& v)
+  {
+    r = v.r;
+    c = v.c;
+  }
+  
+  const Vec2& operator+() const
+  {
+    return *this;
+  }
+  
+  Vec2 operator-() const
+  {
+    return { -r, -c };
+  }
+  
+  Vec2 operator+(const Vec2& v) const
+  {
+    return { r + v.r, c + v.c };
+  }
+  Vec2 operator-(const Vec2& v) const
+  {
+    return { r - v.r, c - v.c };
+  }
+  Vec2& operator+=(const Vec2& v)
+  {
+    this->r += v.r;
+    this->c += v.c;
+    return *this;
+  }
+  Vec2& operator-=(const Vec2& v)
+  {
+    this->r -= v.r;
+    this->c -= v.c;
+    return *this;
+  }
+  Vec2 operator*(float v) const
+  {
+    return { r * v, c * v };
+  }
+  Vec2 operator/(float v) const
+  {
+    return { r / v, c / v };
+  }
+  Vec2& operator/=(float v)
+  {
+    this->r /= v;
+    this->c /= v;
+    return *this;
+  }
+  
+  std::string str() const
+  {
+    return "(" + std::to_string(r) + ", " + std::to_string(c) + ")";
+  }
+};

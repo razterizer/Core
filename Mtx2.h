84a64b81d9f5bb27ab9eea3f97c6d1f1f6a1a389
Rasmus Anthin <scratty@gmail.com>
2024-10-30 19:41:55 +0100

    Mtx2.h: * The first steps.

--- Mtx2.h
+++ Mtx2.h
@@ -5,8 +5,41 @@
 //  Created by Rasmus Anthin on 2024-10-30.
 //
 
-#ifndef Mtx2_h
-#define Mtx2_h
+#pragma once
+#include "Vec2.h"
 
+// /        \
+// | 00  01 |
+// | 10  11 |
+// \        /
 
-#endif /* Mtx2_h */
+// /        \
+// | rr  rc |
+// | cr  cc |
+// \        /
+
+class Mtx2
+{
+  float m00 = 0.f;
+  float m01 = 0.f;
+  float m10 = 0.f;
+  float m11 = 0.f;
+  
+public:
+  Mtx2(float v00, float v01, float v10, float v11)
+    : m00(v00)
+    , m01(v01)
+    , m10(v10)
+    , m11(v11)
+  {}
+  
+  Vec2 operator*(const Vec2& v)
+  {
+    Vec2 ret
+    {
+      m00 * v.r + m01 * v.c,
+      m10 * v.r + m11 * v.c
+    };
+    return ret;
+  }
+};

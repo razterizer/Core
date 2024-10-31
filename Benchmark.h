34b5d0a9247af6bf445004ec19441e97b68be1c9
Rasmus Anthin <scratty@gmail.com>
2024-10-31 19:30:20 +0100

    Benchmark.h: * First implementation.

--- Benchmark.h
+++ Benchmark.h
@@ -5,8 +5,39 @@
 //  Created by Rasmus Anthin on 2024-10-31.
 //
 
-#ifndef Benchmark_h
-#define Benchmark_h
+#pragma once
+#include <chrono>
+#include <string>
+#include <map>
 
 
-#endif /* Benchmark_h */
+#define BM_FUNC(f) [&](){ f; }, #f
+
+template<typename Lambda>
+float calc_time_ms(Lambda&& func)
+{
+  auto start_time = std::chrono::steady_clock::now();
+  func();
+  auto end_time = std::chrono::steady_clock::now();
+  
+  std::chrono::duration<float, std::milli> elapsed_time = end_time - start_time;
+  return elapsed_time.count();
+}
+
+class Benchmark
+{
+  std::map<std::string, float> timers_ms;
+  
+public:
+  template<typename Lambda>
+  void reg(Lambda&& func, const std::string& tag)
+  {
+    timers_ms[tag] = calc_time_ms(func);
+  }
+  
+  void print()
+  {
+    for (const auto& [tag, time_ms] : timers_ms)
+      std::cout << tag << ": " << time_ms << " ms" << std::endl;
+  }
+};

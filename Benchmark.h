ec22e1e435f341c3d00fb2c30cf889fea088f33f
Rasmus Anthin <scratty@gmail.com>
2024-11-01 08:26:55 +0100

    Benchmark.h: * Now only printing the stats in the destructor of Benchmark, to reduce complexity.

--- Benchmark.h
+++ Benchmark.h
@@ -29,7 +29,18 @@ class Benchmark
   std::map<std::string, float> timers_ms;
   std::map<std::string, std::chrono::steady_clock::time_point> start_times;
   
+  void print()
+  {
+    for (const auto& [tag, time_ms] : timers_ms)
+      std::cout << tag << ": " << time_ms << " ms" << std::endl;
+  }
+  
 public:
+  ~Benchmark()
+  {
+    print();
+  }
+
   template<typename Lambda>
   void reg(Lambda&& func, const std::string& tag)
   {
@@ -46,11 +57,5 @@ public:
     auto end_time = std::chrono::steady_clock::now();
     std::chrono::duration<float, std::milli> elapsed_time = end_time - start_times[tag];
     timers_ms[tag] += elapsed_time.count();
-  }
-  
-  void print()
-  {
-    for (const auto& [tag, time_ms] : timers_ms)
-      std::cout << tag << ": " << time_ms << " ms" << std::endl;
   }
 };

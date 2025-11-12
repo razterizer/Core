197c12b8d6205e1f05a8707f616cd0cdc9f9c5eb
Rasmus Anthin <scratty@gmail.com>
2025-11-12 16:28:15 +0100

    Restoring unit_tests.cpp so that it works as normal again. Wohoo!

--- Tests/unit_tests.cpp
+++ Tests/unit_tests.cpp
@@ -5,28 +5,26 @@
 //  Created by Rasmus Anthin on 2024-09-21.
 //
 
-// #FIXME: For some strange reason, Math.h doesn't work in this repo, but it works fine outside the repo.
-
-//#include "DateTime_tests.h"
-//#include "Histogram_tests.h"
-//#include "FlankDetector_tests.h"
-//#include "Math_tests.h"
-//#include <iostream>
+#include "DateTime_tests.h"
+#include "Histogram_tests.h"
+#include "FlankDetector_tests.h"
+#include "Math_tests.h"
+#include <iostream>
 
 
 int main(int argc, char** argv)
 {
-  //std::cout << "### DateTime Tests ###" << std::endl;
-  //datetime::unit_tests();
+  std::cout << "### DateTime Tests ###" << std::endl;
+  datetime::unit_tests();
 
-  //std::cout << "### Histogram Tests ###" << std::endl;
-  //hist::unit_tests();
-  //
-  //std::cout << "### FlankDetector Tests ###" << std::endl;
-  //flankdetector::unit_tests();
-  //
-  //std::cout << "### Math Tests" << std::endl;
-  //math::unit_tests();
+  std::cout << "### Histogram Tests ###" << std::endl;
+  hist::unit_tests();
+  
+  std::cout << "### FlankDetector Tests ###" << std::endl;
+  flankdetector::unit_tests();
+  
+  std::cout << "### Math Tests" << std::endl;
+  math::unit_tests();
   
   return 0;
 }

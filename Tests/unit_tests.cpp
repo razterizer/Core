43f71872dc6ea74ca28e732d3eb16d3a60220ff2
Rasmus Anthin <scratty@gmail.com>
2025-11-13 23:37:02 +0100

    unit_tests.cpp: Now also performs the new benchmark unit tests. * Also improving separators/titles between unit test "modules"/headers. Looks so much neater.

--- Tests/unit_tests.cpp
+++ Tests/unit_tests.cpp
@@ -5,6 +5,7 @@
 //  Created by Rasmus Anthin on 2024-09-21.
 //
 
+#include "Benchmark_tests.h"
 #include "DateTime_tests.h"
 #include "FlankDetector_tests.h"
 #include "Histogram_tests.h"
@@ -14,17 +15,27 @@
 
 int main(int argc, char** argv)
 {
-  std::cout << "### DateTime Tests ###" << std::endl;
+  const int c_sep_width = 79;
+
+  std::cout << str::adjust_str(" Benchmark Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
+  benchmark::unit_tests();
+  std::cout << str::rep_char('=', c_sep_width) << std::endl;
+
+  std::cout << str::adjust_str(" DateTime Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
   datetime::unit_tests();
+  std::cout << str::rep_char('=', c_sep_width) << std::endl;
   
-  std::cout << "### FlankDetector Tests ###" << std::endl;
+  std::cout << str::adjust_str(" FlankDetector Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
   flankdetector::unit_tests();
+  std::cout << str::rep_char('=', c_sep_width) << std::endl;
   
-  std::cout << "### Histogram Tests ###" << std::endl;
+  std::cout << str::adjust_str(" Histogram Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
   hist::unit_tests();
+  std::cout << str::rep_char('=', c_sep_width) << std::endl;
   
-  std::cout << "### MathUtils Tests" << std::endl;
+  std::cout << str::adjust_str(" MathUtils Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
   math::unit_tests();
+  std::cout << str::rep_char('=', c_sep_width) << std::endl;
   
   return 0;
 }

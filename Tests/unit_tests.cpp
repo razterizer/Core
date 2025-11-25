be1cc85b29d77ddb9d9f182f29795a7d98e27e98
Rasmus Anthin <scratty@gmail.com>
2025-11-25 08:36:46 +0100

    Plugging in the unit tests of StringHelper.h into unit_tests.cpp.

--- Tests/unit_tests.cpp
+++ Tests/unit_tests.cpp
@@ -10,6 +10,7 @@
 #include "FlankDetector_tests.h"
 #include "Histogram_tests.h"
 #include "Math_tests.h"
+#include "StringHelper_tests.h"
 #include <iostream>
 
 
@@ -35,6 +36,10 @@ int main(int argc, char** argv)
   
   std::cout << str::adjust_str(" MathUtils Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
   math::unit_tests();
+  std::cout << str::rep_char('=', c_sep_width) << std::endl;
+  
+  std::cout << str::adjust_str(" StringHelper Tests ", str::Adjustment::Center, c_sep_width, 0, '#') << std::endl;
+  str::unit_tests();
   std::cout << str::rep_char('=', c_sep_width) << std::endl;
   
   return 0;

061033cb7ab258a48a750ad16ab48552524de598
Rasmus Anthin <scratty@gmail.com>
2025-12-17 19:54:10 +0100

    build_examples.sh: Implementing a build script for the examples program.

---
+++ Examples/build_examples.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+additional_flags="-I../include/Core/"
+
+../build.sh examples "$1" "${additional_flags[@]}"
+
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
+
+### Post-Build Actions ###

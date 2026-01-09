309c7484f181beec6c8186ac1ab61ae4b2b74867
Rasmus Anthin <scratty@gmail.com>
2026-01-09 09:22:48 +0100

    Term.h: Adding new functions is_console_stdout(), enable_vt_on_stdout(), utf8_to_utf16(), write_console_w() and write_console_a(). Also replacing older GetConsoleMode tests with is_console_stdout() for immense DRYness.

--- include/Core/Term.h
+++ include/Core/Term.h
@@ -7,9 +7,13 @@
 
 #pragma once
 #include "System.h"
+#include "StringHelper.h"
 #include <mutex>    // once_flag, call_once
 #include <clocale>  // setlocale, LC_CTYPE
 #include <cwchar>   // wcwidth (or <wchar.h>)
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 namespace term
 {
@@ -28,15 +32,61 @@ namespace term
     return sys::is_windows_cmd() ? 437 : 65001;
   }
   
+#ifdef _WIN32
+  inline bool is_console_stdout()
+  {
+    DWORD mode = 0;
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (h == INVALID_HANDLE_VALUE) return false;
+    return GetConsoleMode(h, &mode) != 0;
+  }
+  
+  inline bool enable_vt_on_stdout()
+  {
+    if (!is_console_stdout())
+      return false;
+    
+    DWORD newMode = mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    return SetConsoleMode(h, newMode) != 0;
+  }
+  
+  inline std::wstring utf8_to_utf16(std::string_view s)
+  {
+    if (s.empty())
+      return {};
+    
+    int wlen = MultiByteToWideChar(CP_UTF8, 0, s.data(), str::lenI(s)), nullptr, 0);
+    if (wlen <= 0)
+      return {};
+    
+    std::wstring ws;
+    ws.resize((size_t)wlen);
+    MultiByteToWideChar(CP_UTF8, 0, s.data(), str::lenI(s), ws.data(), wlen);
+    return ws;
+  }
+  
+  inline void write_console_w(std::wstring_view ws)
+  {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD written = 0;
+    WriteConsoleW(h, ws.data(), static_cast<DWORD>(ws.size()), &written, nullptr);
+  }
+  
+  inline void write_console_a(std::string_view s)
+  {
+    HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD written = 0;
+    WriteConsoleA(h, s.data(), (DWORD)s.size(), &written, nullptr);
+  }
+#endif
+  
   inline void init_windows_console()
   {
 #ifdef _WIN32
     static bool once [[maybe_unused]] = []()
     {
       // Only if stdout is a console.
-      DWORD mode = 0;
-      HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
-      if (!GetConsoleMode(h, &mode))
+      if (!is_console_stdout())
         return true;
       
       SetConsoleOutputCP(static_cast<UINT>(get_code_page()));

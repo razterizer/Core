3c7242e062526702e3509db7b8411ebb75075935
Rasmus Anthin <scratty@gmail.com>
2026-01-09 09:44:09 +0100

    Term.h: Removing hacky get_code_page() and moving back is_single_column() and henchmen back to Termin8or/Text.h.

--- include/Core/Term.h
+++ include/Core/Term.h
@@ -27,11 +27,6 @@ namespace term
     });
   }
   
-  inline int get_code_page()
-  {
-    return sys::is_windows_cmd() ? 437 : 65001;
-  }
-  
 #ifdef _WIN32
   inline bool is_console_stdout()
   {
@@ -93,43 +88,6 @@ namespace term
       return true;
     }();
 #endif
-  }
-  
-  // We assume single column and rely on encoder fallback.
-  inline bool is_single_column(char32_t cp)
-  {
-    if (sys::is_windows_cmd())
-    {
-      // Treat as single-column if we can encode it to CP437 (or ASCII).
-      // If you don’t have a predicate, just allow and rely on encoding fallback.
-      return cp <= 0x10FFFF;
-    }
-    
-    if (cp > 0x10FFFF)
-      return false;
-    
-    init_locale();
-    wchar_t wc = static_cast<wchar_t>(cp);
-    int w = ::wcwidth(wc);
-    return w == 1; //w <= 1;
-  }
-  
-  inline char32_t get_single_column_char32(char32_t cp)
-  {
-    return is_single_column(cp) ? cp : U'?';
-  }
-  
-  inline std::string encode_single_width_glyph(char32_t preferred,
-                                               char32_t fallback = U'?')
-  {
-    char32_t cp = preferred;
-    if (!is_single_column(cp))
-      cp = fallback;
-    if (!is_single_column(cp))
-      cp = U'?';
-    
-    // Use 437 for cmd.exe for now as we only have a mapping from UTF-8 to CP437 atm.
-    return utf8::encode_char32_codepage(cp, get_code_page());
   }
   
 }
